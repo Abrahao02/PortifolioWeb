36d1c80496921553ad9495b630f0612a48d358aa
Abrahao <edsa.1909@gmail.com>
2024-02-24 01:11:37 -0300

    Portifolio basico pronto
    Todas section, `` prontas ``

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
 // JavaScript para o carrossel
-var swiper = new Swiper(".swiper", {
-    cssMode: true,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    keyboard: true,
-    on: {
-        init: function () {
-            this.slides.forEach(slide => {
-                slide.style.overflow = 'hidden';
-            });
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-    },
-  });
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+    // Adiciona um ouvinte de evento para o botão de próxima imagem
+    document.querySelector('.swiper-button-next').addEventListener('click', function() {
+        swiper.slideNext(); // Navega para a próxima foto
+        updateProjectInfo(swiper.realIndex); // Atualiza as informações do projeto
+    });
+
+    // Adiciona um ouvinte de evento para o botão de imagem anterior
+    document.querySelector('.swiper-button-prev').addEventListener('click', function() {
+        swiper.slidePrev(); // Navega para a foto anterior
+        updateProjectInfo(swiper.realIndex); // Atualiza as informações do projeto
+    });
+
+    // Seleciona todos os títulos (h3) com a classe 'project-title' e adiciona um ouvinte de evento de clique para cada um
+    var projectTitles = document.querySelectorAll('.project-title');
+    projectTitles.forEach(function(title, index) {
+        title.addEventListener('click', function() {
+            swiper.slideTo(index); // Navega para o slide associado ao título clicado
+            updateProjectInfo(index); // Atualiza as informações do projeto
+        });
+    });
+
+    // Atualiza as informações do projeto com base no slide atual
+    function updateProjectInfo(activeIndex) {
+        var projectInfoContainers = document.querySelectorAll('.project-info'); // Seleciona todos os contêineres de informações do projeto
+
+        // Oculta todos os contêineres de informações do projeto
+        projectInfoContainers.forEach(function(container) {
+            container.style.display = 'none';
+        });
+
+        // Exibe o contêiner de informações do projeto correspondente ao slide atual
+        var currentProjectInfoContainer = projectInfoContainers[activeIndex];
+        if (currentProjectInfoContainer) {
+            currentProjectInfoContainer.style.display = 'block';
+        }
+    }
+});
+
+
+
+
+  
 
 
 // NAVEGACAO SUAVE
@@ -54,13 +89,13 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-// APARECE INFO PESSOAIS 
+// APARECE INFO PESSOAIS NOS ICONS
 document.addEventListener("DOMContentLoaded", function() {
     const iconContainers = document.querySelectorAll(".icon-container");
 
     iconContainers.forEach(container => {
         const icon = container.querySelector("i");
-        const info = container.querySelector("p") || container.querySelector("span");
+        const info = container.querySelector("span");
 
         // Adiciona um ouvinte de evento de clique ao ícone para exibir o texto
         icon.addEventListener("click", function() {
@@ -74,6 +109,7 @@ document.addEventListener("DOMContentLoaded", function() {
         info.style.display = info.style.display === "none" ? "inline" : "none";
     }
 });
+
 
 
 
